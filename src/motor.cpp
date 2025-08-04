b77cf1d1a57ec981e3b612d1f060f4433852df41
Jakub Pelc <jakub.pelc@seznam.cz>
2025-08-04 14:00:46 +0200

    motor for esp32 and esp8266

--- src/motor.cpp
+++ src/motor.cpp
@@ -1,37 +1,50 @@
 #include "motor.h"
 
-#ifdef ESP8266
-
 Motor::Motor(int pinIA, int pinIB, int pwmFreq) 
-    : _pinIA(pinIA), _pinIB(pinIB), _pwmFreq(pwmFreq), _state(STOPPED), _speed(0), _stateChanged(true) {
+	: _pinIA(pinIA), _pinIB(pinIB), _pwmFreq(pwmFreq), _state(STOPPED), _speed(0), _stateChanged(true) {
 	pinMode(_pinIA, OUTPUT);
 	pinMode(_pinIB, OUTPUT);
 	// Don't set pin states here, let update() handle it
 }
 
 void Motor::update() {
-    if (!_stateChanged) return;
-    
-    _stateChanged = false;
-    int pwmSpeed = constrain(_speed, 0, 1023);
-    
-    switch (_state) {
-        case FORWARD:
-            analogWriteFreq(_pwmFreq);
-            analogWrite(_pinIA, pwmSpeed);
-            analogWrite(_pinIB, 0);
-            break;
-        case BACKWARD:
-            analogWriteFreq(_pwmFreq);
-            analogWrite(_pinIA, 0);
-            analogWrite(_pinIB, pwmSpeed);
-            break;
-        case STOPPED:
-        default:
-            analogWrite(_pinIA, 0);
-            analogWrite(_pinIB, 0);
-            break;
-    }
+	if (!_stateChanged) return;
+	
+	_stateChanged = false;
+	int pwmSpeed = constrain(_speed, 0, 1023);
+	
+	switch (_state) {
+		case FORWARD:
+			#ifdef ESP8266
+				analogWriteFreq(_pwmFreq);
+			#endif
+
+			#ifdef ESP32
+				analogWriteFrequency(_pwmFreq);
+			#endif
+
+			analogWrite(_pinIA, pwmSpeed);
+			analogWrite(_pinIB, 0);
+			break;
+		case BACKWARD:
+			#ifdef ESP8266
+				analogWriteFreq(_pwmFreq);
+			#endif
+
+			#ifdef ESP32
+				analogWriteFrequency(_pwmFreq);
+			#endif
+
+			analogWrite(_pinIA, 0);
+			analogWrite(_pinIB, pwmSpeed);
+			break;
+			
+		case STOPPED:
+		default:
+			analogWrite(_pinIA, 0);
+			analogWrite(_pinIB, 0);
+			break;
+	}
 }
 
 void Motor::forward(int speed) {
@@ -51,5 +64,3 @@ void Motor::stop() {
 	_state = STOPPED;
 	_stateChanged = true;
 }
-
-#endif // ESP8266
--- src/motor.h
+++ src/motor.h
@@ -1,34 +1,30 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#ifdef ESP8266
-
 #include <Arduino.h>
 #include "Peripheral.h"
 
 class Motor : public Peripheral {
 public:
-    Motor(int pinIA, int pinIB, int pwmFreq = 1000);
-    void update() override;
-    void forward(int speed);
-    void backward(int speed);
-    void stop();
+	Motor(int pinIA, int pinIB, int pwmFreq = 1000);
+	void update() override;
+	void forward(int speed);
+	void backward(int speed);
+	void stop();
 
 private:
-    enum MotorState {
-        STOPPED,
-        FORWARD,
-        BACKWARD
-    };
-    
-    int _pinIA;
-    int _pinIB;
+	enum MotorState {
+		STOPPED,
+		FORWARD,
+		BACKWARD
+	};
+	
+	int _pinIA;
+	int _pinIB;
 	int _pwmFreq;
-    MotorState _state;
-    int _speed;
-    bool _stateChanged;
+	MotorState _state;
+	int _speed;
+	bool _stateChanged;
 };
-
-#endif // ESP8266
 
 #endif // MOTOR_H
