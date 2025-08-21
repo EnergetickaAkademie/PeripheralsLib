3aff721f548140f7f4498b13684b9432c9fbac16
Jiří Svítil <jirka.svitil@gmail.com>
2025-08-21 20:45:15 +0200

    feat: implement MotorSinglePin class with speedup functionality

---
+++ src/motor_single_pin.cpp
@@ -0,0 +1,77 @@
+#include "motor_single_pin.h"
+
+MotorSinglePin::MotorSinglePin(int pinA, int pwmFreq) 
+	: _pinA(pinA), _pwmFreq(pwmFreq), _state(STOPPED), _speed(0), _stateChanged(true),
+	  _speedupEnabled(false), _speedupMultiplier(1.5f), _speedupDuration(300), _isInSpeedup(false), _speedupStartTime(0) {
+	pinMode(_pinA, OUTPUT);
+	// Pin B is connected through resistor to ground, so we don't control it
+}
+
+void MotorSinglePin::update() {
+	if (_isInSpeedup && (millis() - _speedupStartTime >= _speedupDuration)) {
+		_isInSpeedup = false;
+		_stateChanged = true;
+	}
+
+	if (!_stateChanged) return;
+	
+	_stateChanged = false;
+	
+	int actualSpeed = _speed;
+	if (_isInSpeedup && _speedupEnabled) {
+		actualSpeed = constrain((int)(_speed * _speedupMultiplier), 0, 1023);
+	}
+	
+	int pwmSpeed = constrain(actualSpeed, 0, 1023);
+	
+	switch (_state) {
+		case FORWARD:
+			#ifdef ESP8266
+				analogWriteFreq(_pwmFreq);
+			#endif
+
+			#ifdef ESP32
+				analogWriteFrequency(_pwmFreq);
+			#endif
+
+			analogWrite(_pinA, pwmSpeed);
+			break;
+			
+		case STOPPED:
+		default:
+			analogWrite(_pinA, 0);
+			_isInSpeedup = false;
+			break;
+	}
+}
+
+void MotorSinglePin::forward(int speed) {
+	_speed = speed;
+	_state = FORWARD;
+	_stateChanged = true;
+	
+	if (_speedupEnabled) {
+		_isInSpeedup = true;
+		_speedupStartTime = millis();
+	}
+}
+
+void MotorSinglePin::stop() {
+	_speed = 0;
+	_state = STOPPED;
+	_stateChanged = true;
+	_isInSpeedup = false;
+}
+
+void MotorSinglePin::enableSpeedup(bool enabled) {
+	_speedupEnabled = enabled;
+}
+
+void MotorSinglePin::setSpeedupConfig(float multiplier, unsigned long duration_ms) {
+	_speedupMultiplier = multiplier;
+	_speedupDuration = duration_ms;
+}
+
+bool MotorSinglePin::isSpeedupActive() const {
+	return _isInSpeedup;
+}
---
+++ src/motor_single_pin.h
@@ -0,0 +1,36 @@
+#ifndef MOTOR_SINGLE_PIN_H
+#define MOTOR_SINGLE_PIN_H
+
+#include <Arduino.h>
+#include "Peripheral.h"
+
+class MotorSinglePin : public Peripheral {
+public:
+	MotorSinglePin(int pinA, int pwmFreq = 1000);
+	void update() override;
+	void forward(int speed);
+	void stop();
+
+	void enableSpeedup(bool enabled);
+	void setSpeedupConfig(float multiplier, unsigned long duration_ms);
+	bool isSpeedupActive() const;
+
+private:
+	enum MotorState {
+		STOPPED,
+		FORWARD
+	};
+	
+	int _pinA;
+	int _pwmFreq;
+	MotorState _state;
+	int _speed;
+	bool _stateChanged;
+	bool _speedupEnabled;
+	float _speedupMultiplier;
+	unsigned long _speedupDuration;
+	bool _isInSpeedup;
+	unsigned long _speedupStartTime;
+};
+
+#endif // MOTOR_SINGLE_PIN_H
